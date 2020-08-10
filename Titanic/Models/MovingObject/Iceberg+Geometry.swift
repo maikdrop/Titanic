af9cbb71895a1c452932be0d9c0ad40e47e19ed3
Maik <maik_mueller@me.com>
2020-08-10 17:27:42 +0200

    - renamed files

---
+++ Titanic/Models/MovingObject/Iceberg+Geometry.swift
@@ -0,0 +1,26 @@
+//
+//  Iceberg+Geometry.swift
+//  Titanic
+//
+//  Created by Maik on 08.08.20.
+//  Copyright © 2020 maikdrop. All rights reserved.
+//
+
+import Foundation
+
+extension Iceberg {
+
+    struct Point {
+
+        // MARK: - Properties
+        var xCoordinate = 0.0
+        var yCoordinate = 0.0
+    }
+
+    struct Size {
+
+        // MARK: - Properties
+        var width = 0.0
+        var height = 0.0
+    }
+}
