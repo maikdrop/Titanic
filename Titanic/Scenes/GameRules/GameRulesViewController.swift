f5a598662aa2ed3caba7770c01cca55399b240dd
Maik <maik_mueller@me.com>
2020-08-10 20:26:55 +0200

    - added new rules for SwiftLint; - added legal notices

--- Titanic/Scenes/GameRules/GameRulesViewController.swift
+++ Titanic/Scenes/GameRules/GameRulesViewController.swift
@@ -1,42 +1,46 @@
-//
-//  GameRulesViewController.swift
-//  Titanic
-//
-//  Created by Maik on 03.04.20.
-//  Copyright © 2020 maikdrop. All rights reserved.
-//
+/*
+ MIT License
+
+Copyright (c) 2020 Maik Müller (maikdrop) <maik_mueller@me.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 
 import UIKit
 
 final class GameRulesViewController: UIViewController {
 
     // MARK: - Properties
-    @IBOutlet weak var subheadlineLbl: UILabel! {
+    @IBOutlet private weak var subheadlineLbl: UILabel! {
         didSet {
             subheadlineLbl.text = AppStrings.Rules.subheadlineTitle
         }
     }
 
-    @IBOutlet weak var goalTitleLbl: UILabel! {
+    @IBOutlet private weak var goalTitleLbl: UILabel! {
         didSet {
             goalTitleLbl.attributedText = NSAttributedString(string: AppStrings.Rules.goalSectionTitle, attributes:
             [.underlineStyle: NSUnderlineStyle.single.rawValue])
         }
     }
 
-    @IBOutlet weak var goalContentLbl: UILabel! {
+    @IBOutlet private weak var goalContentLbl: UILabel! {
         didSet {
             goalContentLbl.text = AppStrings.Rules.goalSectionContent
         }
     }
 
-    @IBOutlet weak var usageTitleLbl: UILabel! {
+    @IBOutlet private weak var usageTitleLbl: UILabel! {
         didSet {
             usageTitleLbl.attributedText = NSAttributedString(string: AppStrings.Rules.usageSectionTitle, attributes:
             [.underlineStyle: NSUnderlineStyle.single.rawValue])
         }
     }
-    @IBOutlet weak var usageContentLbl: UILabel! {
+    @IBOutlet private weak var usageContentLbl: UILabel! {
         didSet {
             usageContentLbl.text = AppStrings.Rules.usageSectionContent
             //.reduce("",+).replacingOccurrences(of: "|", with: "\n")
